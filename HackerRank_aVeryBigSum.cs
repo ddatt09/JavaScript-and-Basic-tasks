5598745ce9e2e37b6580ff88d98ba929bb5f1ca5
DatDatt <75371408+ddatt09@users.noreply.github.com>
2021-04-04 11:07:56 +0700

    Create HackerRank_aVeryBigSum.cs

---
+++ HackerRank_aVeryBigSum.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        /*
+Trong thử thách này, bạn được yêu cầu tính toán và in ra tổng các phần tử trong một mảng, hãy nhớ rằng một số số nguyên đó có thể khá lớn.
+
+FUNCTION DESCRIPTION:
+    Hoàn thành hàm aVeryBigSum trong trình chỉnh sửa bên dưới. Nó phải trả về tổng của tất cả các phần tử mảng.
+
+aVeryBigSum có (các) tham số sau:
+    int ar [n]: một mảng các số nguyên.
+
+RETURN:
+    long: tổng của tất cả các phần tử mảng
+
+INPUT FORMAT:
+    Dòng đầu tiên của đầu vào bao gồm một số nguyên.
+    Dòng tiếp theo chứa các số nguyên được phân tách bằng dấu cách chứa trong mảng.
+
+OUTPUT FORMAT:
+    Trả về tổng số nguyên của các phần tử trong mảng.
+
+CONSTRAINTS(ràng buộc):
+    1 <= n <= 10
+    0 <= ar[i] <= 10^10
+
+SAMPLE INPUT:
+    5
+    1000000001 1000000002 1000000003 1000000004 1000000005
+SAMPLE OUTPUT:
+    5000000015
+
+NOTES:
+    Phạm vi của số nguyên 32 bit là: (-2^31) tới (2^31 - 1) hoặc [-2147483648, 2147483647].
+    Khi chúng tôi thêm một số giá trị số nguyên, tổng kết quả có thể vượt quá phạm vi trên. Bạn có thể cần sử dụng int long C / C ++ / Java để lưu trữ các tổng như vậy.
+                */
+        static void Main(string[] args)
+        {
+            long aVeryBigSum(long[] ar)
+            {
+
+                return ar.Sum();
+            }
+
+        int arCount = Convert.ToInt32(Console.ReadLine());
+
+            long[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt64(arTemp))
+            ;
+            long result = aVeryBigSum(ar);
+
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    }
+}
