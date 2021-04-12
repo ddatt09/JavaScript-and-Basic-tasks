4e74c456b921ed4892a20157d4ac68d7800017d6
Hoang Cuong <75371408+hoangcuong2k1@users.noreply.github.com>
2021-04-13 03:56:13 +0700

    Update HR[19]_py_BillDivision.py

--- HR[19]_py_BillDivision.py
+++ HR[19]_py_BillDivision.py
@@ -1,1 +1,28 @@
+"""
+.................................................Bill Division ...............................................
 
+LINK ORIGIN: https://www.hackerrank.com/challenges/bon-appetit/problem
+
+"""
+def bonAppetit(bill, k, b):
+    sumShare = sum(bill) - bill[k]
+    annaPays = sumShare // 2
+    if b == annaPays:
+        print('Bon Appetit')
+        return
+
+    brianRest = abs(b - annaPays)
+    print(brianRest)
+    
+    if __name__ == '__main__':
+    nk = input().rstrip().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    bill = list(map(int, input().rstrip().split()))
+
+    b = int(input().strip())
+
+    bonAppetit(bill, k, b)
