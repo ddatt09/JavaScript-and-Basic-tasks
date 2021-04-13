0aafdce122282b5ddc6d10299721eeaa3c9d1937
Hoang Cuong <75371408+hoangcuong2k1@users.noreply.github.com>
2021-04-13 10:46:09 +0700

    Update HR[22]_py_CountingValleys.py

--- HR[22]_py_CountingValleys.py
+++ HR[22]_py_CountingValleys.py
@@ -1,1 +1,29 @@
 
+"""
+................................................. Counting Valleys ...............................................
+
+TASK LINK: https://www.hackerrank.com/challenges/counting-valleys/problem
+
+"""
+
+
+def countingValleys(steps, path):
+    compareWithSeaLevel = 0
+    numberOfValleys = 0
+
+    for i in range(len(path)):
+        if path[i] == "D": compareWithSeaLevel -= 1
+        if path[i] == "U": compareWithSeaLevel += 1
+        if compareWithSeaLevel == 0 and path[i] == "U": numberOfValleys += 1
+
+    return numberOfValleys
+
+
+if __name__ == '__main__':
+    steps = int(input().strip())
+
+    path = input()
+
+    result = countingValleys(steps, path)
+
+    print(str(result) + '\n')
